76934c5686f4c1365d10980bdeceb6ad16e23ac2
Joarell <josephjoarell@gmail.com>
2025-03-13 18:28:08 -0300

    refactor (update): removes the asset from the worker

--- app/otto/app/installation.handler.mjs
+++
@@ -1,8 +0,0 @@
-export async function installer() {
-	globalThis.hideInstallPromotion();
-	globalThis.deferredPrompt.prompt();
-
-	const { outcome } = await deferredPrompt.userChoice;
-	console.log(`User response to the install prompt: ${outcome}`);
-	globalThis.deferredPrompt = null;
-};
